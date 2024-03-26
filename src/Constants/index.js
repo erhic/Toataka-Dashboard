4048c667987ebfaca08f1ef292ce88a6e7ffda69
erhic <ericngugi24@gmail.com>
2024-03-26 15:45:35 +0300

    feat : set navmenu link constants

---
+++ src/Constants/index.js
@@ -0,0 +1,12 @@
+export const sideNavMenu = [
+  { image: "", name: "Dashboard" },
+  { image: "", name: "Orders" },
+  { image: "", name: "Client" },
+  { image: "", name: "Contact" },
+  { image: "", name: "Task" },
+  { image: "", name: "Caledar" },
+  { image: "", name: "Contacts" },
+  { image: "", name: "Analytics" },
+  { image: "", name: "Reports" },
+  { image: "", name: "Settings" },
+];
